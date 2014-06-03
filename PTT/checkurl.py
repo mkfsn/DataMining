f601a350dd4f60b067ab1af8eb96817e38aa8aa8
mkfsn <p408865@gmail.com>
2014-06-03 15:10:54 +0800

    store status code to db

--- PTT/checkurl.py
+++ PTT/checkurl.py
@@ -24,32 +24,31 @@ for url in infile:
   url = url.split('\n')[0]
 
   page = re.match("^[0-9]",url)
-  ppturl = re.match("^http://ppt.cc", url) 
-  goourl = re.match("^http://goo.gl", url) 
-  orzurl = re.match("^http://0rz.tw", url) 
-  youtuurl = re.match("^http://youtu.be", url) 
-  fburl = re.match("^http://fb.com", url) 
-  bitlyurl = re.match("^http://bit.ly", url) 
-  bitlyurl2 = re.match("^http://bitly.com", url) 
-  tinyurl = re.match("^http://tinyurl.com", url) 
-  xcourl = re.match("^http://x.co", url)
+  ppturl = re.match("^http://ppt.cc/", url)
+  goourl = re.match("^http://goo.gl/", url)
+  orzurl = re.match("^http://0rz.tw/", url)
+  youtuurl = re.match("^http://youtu.be/", url)
+  fburl = re.match("^http://fb.com/", url)
+  bitlyurl = re.match("^http://bit.ly/", url)
+  bitlyurl2 = re.match("^http://bitly.com/", url)
+  tinyurl = re.match("^http://tinyurl.cm/", url)
+  xcourl = re.match("^http://x.co/", url)
 
   if page : 
     outfile.write(url)
     print url
+    continue
 
-  if ppturl or goourl or orzurl or youtuurl or fburl or bitlyurl or bitlyurl2 or tinyurl or xcourl:
+  flag = False
+  if flag or ppturl or goourl or orzurl or youtuurl or fburl or bitlyurl or bitlyurl2 or tinyurl or xcourl:
     # Search database
     cursor.execute('SELECT * FROM urls WHERE entry = ?', (url,))
     result = cursor.fetchall()
 
     if len(result) == 0:
       shurl = shorturl.redirectURL(url)
-      if shurl is not None:
-        # outfile.write(shurl)
-        cursor.execute("INSERT INTO urls VALUES (?,?)", (url, shurl,))
-      else:
-        cursor.execute("INSERT INTO urls VALUES (?,?)", (url, '',))
+      # ShortURL, OriginURL, StatusCode
+      cursor.execute("INSERT INTO urls VALUES (?,?,?)", (url, shurl['url'], shurl['status'],))
       con.commit()
     elif  result[0][1] is '':
       pass
